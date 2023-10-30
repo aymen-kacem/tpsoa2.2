66091552c845af97117f3a52ab11bb002c33d6f6
aymen-kacem <aymenkacem2019@gmail.com>
2023-10-30 19:50:34 +0100

    commit

--- src/ex2/Main.java
+++ src/ex2/Main.java
@@ -6,9 +6,11 @@ public class Main {
 		// TODO Auto-generated method stub
 		BanqueService b =new BanqueService();
 		System.out.println("le montant est :"+b.conversion(120));
-		System.out.println("le compte est:"+b.getCompte().code+b.getCompte().solde+b.getCompte().dateCreation);
+		System.out.println("le compte est:"+b.getCompte().code+b.getCompte().solde+b.getCompte()
+		.dateCreation);
 		for(int i=0;i<5;i++) {
-			System.out.println("le compte"+i+""+b.getComptes().get(i).code+b.getComptes().get(i).solde+b.getComptes().get(i).dateCreation);// get et toString
+			System.out.println("le compte"+i+""+b.getComptes().get(i).code+b.getComptes()
+			.get(i).solde+b.getComptes().get(i).dateCreation);// get et toString
 		}
 		 //List<String> color = Arrays.asList("Blue", "Red", "Green"); equivalant a add 
 
